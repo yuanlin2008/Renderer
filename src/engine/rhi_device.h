3edf5b5c0d870828e5075a73961d1f61d1c10a68
yuanlin2008 <yuanlin2008@hotmail.com>
2024-10-24 01:14:10 +0800

    123

--- src/engine/rhi_device.h
+++ src/engine/rhi_device.h
@@ -1,24 +1,7 @@
 #pragma once
 
 #include "rhi_context.h"
-
-struct RHIBuffer {};
-struct RHITexture {};
-struct RHISampler {};
-struct RHIVertexFormat {};
-struct RHICommandQueue {};
-struct RHICommandQueueFamily {};
-struct RHICommandPool {};
-struct RHICommandBuffer {};
-struct RHISwapChain {};
-struct RHIFramebuffer {};
-struct RHIShader {};
-struct RHIUniformSet {};
-struct RHIPipeline {};
-struct RHIRenderPass {};
-struct RHIQueryPool {};
-struct RHIFence {};
-struct RHISemaphore {};
+#include "rhi_types.h"
 
 class RHIDevice {
 public:
---
+++ src/engine/rhi_types.h
@@ -0,0 +1,19 @@
+#pragma once
+
+struct RHIBuffer {};
+struct RHITexture {};
+struct RHISampler {};
+struct RHIVertexFormat {};
+struct RHICommandQueue {};
+struct RHICommandQueueFamily {};
+struct RHICommandPool {};
+struct RHICommandBuffer {};
+struct RHISwapChain {};
+struct RHIFramebuffer {};
+struct RHIShader {};
+struct RHIUniformSet {};
+struct RHIPipeline {};
+struct RHIRenderPass {};
+struct RHIQueryPool {};
+struct RHIFence {};
+struct RHISemaphore {};
