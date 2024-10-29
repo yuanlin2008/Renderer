7d3adefd943a0ba6c4c8564a2fb115c7909a465e
yuanlin2008 <yuanlin2008@hotmail.com>
2024-10-30 00:15:48 +0800

    123

--- src/engine/rhi_device.h
+++ src/engine/rhi_device.h
@@ -1,9 +1,41 @@
 #pragma once
 
 #include "rhi_context.h"
-#include "rhi_types.h"
 
 class RHIDevice {
 public:
 	virtual ~RHIDevice() {}
+
+	enum class CommandQueueType {
+		Graphics,
+		Compute,
+		Transfer,
+	};
+	struct CommandQueue {};
+	struct CommandPool {};
+	struct CommandBuffer {};
+	struct Buffer {};
+	struct Texture {};
+	struct Sampler {};
+	struct VertexFormat {};
+	struct SwapChain {};
+	struct Framebuffer {};
+	struct Shader {};
+	struct UniformSet {};
+	struct Pipeline {};
+	struct RenderPass {};
+	struct QueryPool {};
+	struct Fence {};
+	struct Semaphore {};
+
+	virtual CommandQueue *get_command_queue(CommandQueueType type) = 0;
+
+	virtual CommandPool *create_command_pool(CommandQueueType type) = 0;
+	virtual void destroy_command_pool(CommandPool *pool) = 0;
+
+	virtual CommandBuffer *alloc_command_buffer(CommandPool *pool) = 0;
+	virtual void begin_command_buffer(CommandBuffer *cmd_buffer) = 0;
+	virtual void end_command_buffer(CommandBuffer *cmd_buffer) = 0;
+
+	virtual Fence *create_fence() = 0;
 };
