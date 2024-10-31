d320477a85f9ffe126d89708132bf53908157748
yuanlin2008 <yuanlin2008@hotmail.com>
2024-10-31 15:21:16 +0800

    123

--- src/engine/rhi_device.h
+++ src/engine/rhi_device.h
@@ -6,11 +6,9 @@ class RHIDevice {
 public:
 	virtual ~RHIDevice() {}
 
-	enum class CommandQueueType {
-		Graphics,
-		Compute,
-		Transfer,
-	};
+	/******************************
+	 * RHI Objects.
+	 ******************************/
 	struct CommandQueue {};
 	struct CommandPool {};
 	struct CommandBuffer {};
@@ -28,11 +26,48 @@ public:
 	struct Fence {};
 	struct Semaphore {};
 
+	/******************************
+	 * Memory
+	 ******************************/
+	enum class MemoryAllocationType {
+		CPU, // For images, CPU allocation also means linear, GPU is tiling optimal.
+		GPU,
+	};
+
+	/******************************
+	 * Buffer
+	 ******************************/
+	enum class BufferUsage {
+		TRANSFER_SRC = (1 << 0),
+		TRANSFER_DST = (1 << 1),
+		UNIFORM_TEXEL = (1 << 2),
+		UNIFORM_BUFFER = (1 << 4),
+		STORAGE_BUFFER = (1 << 5),
+		INDEX_BUFFER = (1 << 6),
+		VERTEX_BUFFER = (1 << 7),
+		INDIRECT_BUFFER = (1 << 8),
+	};
+
+	/******************************
+	 * Command Queue.
+	 ******************************/
+	enum class CommandQueueType {
+		Graphics,
+		Compute,
+		Transfer,
+	};
+
 	virtual CommandQueue *get_command_queue(CommandQueueType type) = 0;
 
+	/******************************
+	 * Command Pool.
+	 ******************************/
 	virtual CommandPool *create_command_pool(CommandQueueType type) = 0;
 	virtual void destroy_command_pool(CommandPool *pool) = 0;
 
+	/******************************
+	 * Command Pool.
+	 ******************************/
 	virtual CommandBuffer *alloc_command_buffer(CommandPool *pool) = 0;
 	virtual void begin_command_buffer(CommandBuffer *cmd_buffer) = 0;
 	virtual void end_command_buffer(CommandBuffer *cmd_buffer) = 0;
